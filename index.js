70cfe696a6af653747d052e553744fc652b05e7f
ShivaRK <shivarkcodes@gmail.com>
2023-01-16 22:11:13 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
+app.get('/',(req,res)=>{
+    res.send("hello!!")
+})
+
 app.listen(port,()=>{
     console.log("Listening to port at 5000");
 })
