b7eb02fa6ffda4a3797110895a07025e62e26a22
ShivaRK <shivarkcodes@gmail.com>
2023-01-22 01:16:27 +0530

    modified:   index.js 	new file:   public/index.html 	new file:   views/404.html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express();
 const port = 5000;
 const cors = require('cors')
+const path = require('path')
 
 const corsOptions = {
     origin: '*',
@@ -10,9 +11,8 @@ const corsOptions = {
     credentials: true
 }
 
-app.get('/',(req,res)=>{
-    res.status(200).send("hello!!")
-})
+app.use('/', express.static(path.join(__dirname, 'public')))
+
 
 connectToMongo();
 app.use(cors(corsOptions))
@@ -25,6 +25,17 @@ app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port,()=>{
     console.log("Listening to port at 5000");
+})
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
 })
 
 module.exports = app;
